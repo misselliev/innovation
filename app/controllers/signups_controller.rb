dfab91d77806554c06ad4c0b7c1f582ed53d7abb
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-15 20:02:21 -0700

    Landing page for festival

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class SignupsController < ApplicationController
+  def new
+    @signup = Signup.new
+  end
+
+  def create
+    @signup = Signup.new(signup_params)
+    if @signup.save
+      redirect_to thanks_path
+    else
+      render :new
+  end
+end
+
+  private
+
+  def signup_params
+    params.require(:signup).permit(:email)
+  end
+end
---
+++ db/migrate/20190516020128_create_signups.rb
@@ -0,0 +1,9 @@
+class CreateSignups < ActiveRecord::Migration[5.2]
+  def change
+    create_table :signups do |t|
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
